6d04b3c3fdbf634e1ff4a3a93d196af4c4df2014
Eugene Tischenko <mr.hadingus@gmail.com>
2020-04-05 01:02:11 +0500

    Added gamemode

--- game_lib.py
+++ game_lib.py
@@ -5,19 +5,18 @@ from abc import ABC, abstractmethod
 class Unit:
     __next_id = 0
 
-    def __init__(self, category, mover):
+    def __init__(self, mover):
         self.id = Unit.__next_id
-        self.type = category
         self.mover = mover
 
     def __copy__(self):
-        new_unit = self.__class__(self.type, self.mover)
+        new_unit = self.__class__(self.mover)
         new_unit.__dict__.update(self.__dict__)
         new_unit.mover = copy.copy(self.mover)
         return new_unit
 
     def __deepcopy__(self, memo={}):
-        new_unit = self.__class__(self.type, self.mover)
+        new_unit = self.__class__(self.mover)
         new_unit.__dict__.update(self.__dict__)
         new_unit.mover = copy.deepcopy(self.mover, memo)
         return new_unit
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from copy import deepcopy
 from game_lib import *
+from gamemode import *
 
 someMovers = [PawnMover(), KingMover(), FlexMover(), SwapMover()]
 
 units = []
 
 for i in range(8):
-    units.append(Unit(str(i), someMovers[i % 4]))
+    units.append(Unit(someMovers[i % 4]))
 
 for unit in units:
     unit.move(None, None)
@@ -24,7 +25,19 @@ for i in range(8):
 unitSet = set(units)
 print("Len of double set", len(units))
 
-for i in range(8):
-    assert units[i].type == units[i + 8].type
-
 print("All is correct")
+
+# GameMode Test
+
+dir = Director()
+b = (ClassicModeBuilder(), AdvancedModeBuilder(), TriangleModeBuilder(),
+                            AllUnitsModeBuilder(), KingPoliceModeBuilder(), WallModeBuilder())
+
+modes = [dir.construct_game_mode(md) for md in b]
+for m in modes:
+    print(m.size_map)
+
+print("GameMode is correct")
+
+
+
