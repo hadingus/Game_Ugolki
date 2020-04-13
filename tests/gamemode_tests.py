79f4f1b400ad40caf1056eed0edc538511ba8de9
Eugene Tischenko <mr.hadingus@gmail.com>
2020-04-14 00:21:32 +0500

    Added tests for gamemode

--- tests/gamemode_tests.py
+++ tests/gamemode_tests.py
@@ -1,7 +1,7 @@
 from unit import Type
 from itertools import product
 
-from gamemode import Director, ClassicModeBuilder, AllUnitsModeBuilder
+from gamemode import Director, ClassicModeBuilder, AllUnitsModeBuilder, TriangleModeBuilder
 
 
 def test_classic():
@@ -19,3 +19,20 @@ def test_classic():
 
     print(len(positions))
     assert len(positions) == 9
+
+
+def test_triangle_builder():
+    director = Director()
+    mode = director.construct_game_mode(TriangleModeBuilder())
+
+    assert mode.size_map == 8
+
+    positions = set()
+
+    for unit, x, y in mode.arrangement:
+        assert 0 <= x <= 3 and 0 <= y <= 3
+        assert unit.type == Type.FLEX
+        positions.add((x, y))
+
+    print(len(positions))
+    assert len(positions) == 10
