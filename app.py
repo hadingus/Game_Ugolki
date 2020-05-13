86cf9c4c09976e703b7e3b5b8b1a6310e0cb8e76
Viacheslav Surkov <surokpro@gmail.com>
2020-05-13 17:25:00 +0300

    main

---
+++ app.py
@@ -0,0 +1,29 @@
+import pygame
+from gui.gui_operator import GuiOperator
+from gui.start_page import StartPage
+
+
+class App:
+    def start(self):
+        pygame.init()
+        screen = pygame.display.set_mode([800, 700])
+
+        pygame.display.flip()
+
+        operator = GuiOperator()
+        startPage = StartPage(screen, operator)
+        operator.state = startPage
+
+        working = True
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    working = False
+                    break
+                operator.state.handle(event)
+                operator.state.draw()
+                pygame.display.flip()
+            if not working:
+                break
--- main.py
+++ main.py
@@ -1,51 +1,9 @@
-from copy import deepcopy
-from unit import *
-from gamemode import *
-from gui.components import Button
-from gui.start_page import StartPage
-from gui.mode_page import ModePage
-from gui.gui_operator import GuiOperator
-from gui.board_page import BoardPage
-import pygame
-
-from board import Board
+from app import App
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode([800, 700])
-
-    pygame.display.flip()
-
-    director = Director()
-    mode = director.construct_game_mode(ClassicModeBuilder())
-    board = Board(mode)
-
-    operator = GuiOperator()
-    startPage = StartPage(screen, operator)
-    # operator.state = BoardPage(screen, operator, mode)
-    operator.state = startPage
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit(0)
-            operator.state.handle(event)
-            operator.state.draw()
-            pygame.display.flip()
-
-    board = Board(dir.construct_game_mode(b[0]))
-    board.print_board()
-
-    print("First board is correct")
-
-    board.reformat(dir.construct_game_mode(b[3]))
-    board.print_board()
-
-    print("Second board is correct")
-
+    App().start()
 
 
 if __name__ == '__main__':
     main()
-
